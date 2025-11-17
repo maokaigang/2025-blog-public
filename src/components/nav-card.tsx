699c1843974cc3a25d580f75d43e4a1ee73ed44b
maokaigang <1424800269@qq.com>
2025-11-17 22:37:53 +0800

    fix img

--- src/components/nav-card.tsx
+++ src/components/nav-card.tsx
@@ -129,8 +129,8 @@ export default function NavCard() {
 				y={position.y}
 				className={clsx('overflow-hidden', form === 'mini' && 'p-3', form === 'icons' && 'flex items-center gap-6 p-3')}>
 				<Link className='flex items-center gap-3' href='/'>
-					<Image src='/images/avatar.png' alt='avatar' width={40} height={40} style={{ boxShadow: ' 0 12px 20px -5px #E2D9CE' }} className='rounded-full' />
-					{form === 'full' && <span className='font-averia mt-1 text-2xl leading-none font-medium'>Mao</span>}
+					<Image src='/images/look.jpg' alt='avatar' width={40} height={40} style={{ boxShadow: ' 0 12px 20px -5px #E2D9CE' }} className='rounded-full' />
+					{form === 'full' && <span className='font-averia mt-1 text-2xl leading-none font-medium'>比格沃斯</span>}
 					{form === 'full' && <span className='text-brand mt-2 text-xs font-medium'>(思考中)</span>}
 				</Link>
 
